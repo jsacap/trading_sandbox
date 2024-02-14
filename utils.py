f5b6cd737cfa02af6293182bc3448f8ac1a91176
jsacap <sanchoalegre@gmail.com>
2024-02-14 16:12:21 +1100

    jsax backtesting trades df

---
+++ utils.py
@@ -0,0 +1,10 @@
+def get_hist_data_filename(pair, granularity):
+    return f'historical_data/{pair}_{granularity}.pkl'
+
+
+def get_instruments_data_filename():
+    return 'instruments.pkl'
+
+
+if __name__ == '__main__':
+    pass
