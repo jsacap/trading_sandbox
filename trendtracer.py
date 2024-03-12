d12c5c11dd2494595802565ace731bb4a6daaea5
jsacap <sanchoalegre@gmail.com>
2024-03-13 09:05:23 +1100

    SMA JPY - TEST

--- trendtracer.py
+++ trendtracer.py
@@ -9,14 +9,14 @@ import matplotlib.pyplot as plt
 # , delimiter='\t', names=['Open', 'High', 'Low', 'Close', 'Volume'], header=0
 
 # Darwinex spreadsheet
-df = pd.read_csv('../Data/Darwinex/GBPUSD60.csv', header=None,
+df = pd.read_csv('../Data/Darwinex/USDJPY60.csv', header=None,
                  names=['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume'])
 df['DateTime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
 df.set_index('DateTime', inplace=True)
 df.drop(columns=['Date', 'Time'], inplace=True)
 
 
-df = df.iloc[-300:]
+df = df.iloc[-120:]
 # df = df.iloc[190:224]
 
 # Init
@@ -34,16 +34,16 @@ df['Swing_High'] = df['High'].iloc[0]
 df['Trend'] = 'Neutral'
 df['Bar_Number'] = None
 
-higher_high = df['High'].iloc[0]
-lower_low = df['Low'].iloc[0]
+higher_high = None
+lower_low = None
 
 bullish_pullback_count = 0
 bearish_pullback_count = 0
 bullish_pullback = False
 bearish_pullback = False
 
-swing_high = df['High'].iloc[0]
-swing_low = df['Low'].iloc[0]
+swing_high = None
+swing_low = None
 
 uptrend_lowest_pullback = None
 downtrend_highest_pullback = None
@@ -71,22 +71,30 @@ for i, row in df.iloc[1:].iterrows():
 
     # clear_output()
     # Trend rotation
-    if current_close > swing_high:
+    if current_trend == "Neutral":
+        if swing_low is None and swing_high is None:
+            swing_high = current_high
+            swing_low = current_low
+            higher_high = current_high
+            lower_low = current_low
+
+    if current_close > swing_high and current_close > higher_high:
         current_trend = 'Uptrend'
-        bearish_pullback_count = 0
-        bearish_pullback = False
-    elif current_close < swing_low:
-        current_trend = 'Downtrend'
         bullish_pullback_count = 0
         bullish_pullback = False
+        bearish_pullback_count = 0
+        bearish_pullback = False
+        if uptrend_lowest_pullback is not None:
+            swing_low = uptrend_lowest_pullback
+    elif current_close < swing_low and current_low < lower_low:
+        current_trend = 'Downtrend'
+        bearish_pullback_count = 0
+        bearish_pullback = False
+        bullish_pullback_count = 0
+        bullish_pullback = False
+        if downtrend_highest_pullback is not None:
+            swing_high = downtrend_highest_pullback
     # Finding the range in the beginning of the dataset
-    elif current_trend == "Neutral":
-        if swing_high is not None:
-            if current_high > previous_high:
-                swing_high = current_high
-        if swing_low is not None:
-            if current_low < previous_low:
-                swing_low = current_low
 
     df.at[i, 'Trend'] = current_trend
 
@@ -97,38 +105,34 @@ for i, row in df.iloc[1:].iterrows():
 
     df.at[i, 'Price_Status'] = price_status
     # ================================================================================================== #
-    if current_trend == 'Uptrend':
-        if current_close > swing_high:
-            bullish_pullback_count = 0
-            bullish_pullback = False
-            if uptrend_lowest_pullback is not None:
-                swing_low = uptrend_lowest_pullback
-        if current_high > previous_high and current_high > swing_high and not bullish_pullback:
-            swing_high = current_high
-        if current_close < swing_high and candle_type == 'Bear':
-            bullish_pullback_count += 1
 
-        if bullish_pullback_count >= 2:
+    if current_trend == 'Uptrend':
+        if current_high > previous_high and current_low > swing_high and not bullish_pullback:
+            if higher_high is not None:
+                higher_high = max(higher_high, current_high)
+            else:
+                higher_high = current_high
+        if current_close < higher_high and candle_type == 'Bear':
+            bullish_pullback_count += 1
+        if bullish_pullback >= 2:
             bullish_pullback = True
-        else:
-            bullish_pullback = False
+            swing_high = higher_high
 
         if bullish_pullback and current_close > swing_low:
             if current_low < previous_low:
                 uptrend_lowest_pullback = current_low
 
     elif current_trend == 'Downtrend':
-        if current_close < swing_low:
-            if downtrend_highest_pullback is not None:
-                swing_high = downtrend_highest_pullback
-            bearish_pullback_count = 0
-            bearish_pullback = False
         if current_low < previous_low and current_low < swing_low and not bearish_pullback:
-            swing_low = current_low
-        if current_close > swing_low and candle_type == 'Bull':
+            if lower_low is not None:
+                lower_low = min(lower_low, current_low)
+            else:
+                lower_low = current_low
+        if current_close > lower_low and candle_type == 'Bull':
             bearish_pullback_count += 1
         if bearish_pullback_count >= 2:
             bearish_pullback = True
+            swing_low = lower_low
 
         if bearish_pullback and current_close < swing_high:
             if current_high > previous_high:
@@ -160,6 +164,19 @@ for i, row in df.iloc[1:].iterrows():
     # fig.add_trace(go.Scatter(x=downtrend_markers.index, y=downtrend_markers['High'] + 0.001 * downtrend_markers['High'],
     #               mode='markers', name='Downtrend', marker=dict(symbol='triangle-down', color='orange', size=8)))
 
+    # # Add Swing High and Swing Low markers for the current row
+    # fig.add_trace(go.Scatter(x=[i], y=[row['Swing_High']],
+    #                          mode='markers', name='Swing High',
+    #                          marker=dict(symbol='circle',
+    #                                      color='green', size=14),
+    #                          showlegend=False))
+
+    # fig.add_trace(go.Scatter(x=[i], y=[row['Swing_Low']],
+    #                          mode='markers', name='Swing Low',
+    #                          marker=dict(symbol='circle',
+    #                                      color='red', size=14),
+    #                          showlegend=False))
+
     # fig.update_layout(
     #     template='plotly_dark',
     #     xaxis=dict(showgrid=False),
@@ -189,6 +206,20 @@ fig.add_trace(go.Scatter(x=uptrend_markers.index, y=uptrend_markers['Low'] - 0.0
               mode='markers', name='Uptrend', marker=dict(symbol='triangle-up', color='blue', size=8)))
 fig.add_trace(go.Scatter(x=downtrend_markers.index, y=downtrend_markers['High'] + 0.001 * downtrend_markers['High'],
               mode='markers', name='Downtrend', marker=dict(symbol='triangle-down', color='orange', size=8)))
+
+
+# Add Swing High and Swing Low markers for the current row
+fig.add_trace(go.Scatter(x=df.index, y=df['Swing_High'],
+                         mode='markers', name='Swing High',
+                         marker=dict(symbol='circle',
+                                     color='green', size=14),
+                         showlegend=False))
+
+fig.add_trace(go.Scatter(x=df.index, y=df['Swing_Low'],
+                         mode='markers', name='Swing Low',
+                         marker=dict(symbol='circle',
+                                     color='red', size=14),
+                         showlegend=False))
 
 # Update the layout for a clearer view
 fig.update_layout(title='Candlestick Chart with Swing Highs and Lows',
